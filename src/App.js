ee962c58b95f356002a1c5f4e61581f6afc92d12
Kandukuru Udaya Sankar <udayasankar@Kandukurus-MacBook-Pro.local>
2022-11-21 22:26:02 +0530

    Demo gitleaks

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+   const BUNDLE_ENTERPRISE__CONTRIBSYS__COM="cafebabe:deadbeef"  
+const accessToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'; 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Demo App
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
